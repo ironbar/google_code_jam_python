c7d903514216867e42a6f373a5520280ca020656
gbarbadillo <gbarbadillo@das-nano.com>
2022-12-03 13:27:04 +0100

    first version of how sum

---
+++ practice/how_sum.py
@@ -0,0 +1,17 @@
+"""
+https://www.youtube.com/watch?v=oBt53YbR9Kk&t=1638s
+"""
+import functools
+
+@functools.lru_cache(maxsize=None)
+def how_sum(target, number, numbers):
+    if target < 0:
+        return None
+    elif target == 0:
+        return []
+    for number in numbers:
+        ret = how_sum(target - number, number, numbers)
+        if ret is not None:
+            return ret + [number]
+
+print(how_sum(47, 0, (5, 3)))
