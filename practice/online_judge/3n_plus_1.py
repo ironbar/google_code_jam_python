1be1356395eeb202ceec6a7b87b8ada85d71a1c0
gbarbadillo <gbarbadillo@das-nano.com>
2023-01-13 08:40:25 +0100

    simplified implementation

--- practice/online_judge/3n_plus_1.py
+++ practice/online_judge/3n_plus_1.py
@@ -3,22 +3,22 @@ from functools import lru_cache
 
 
 @lru_cache(maxsize=None)
-def get_3n_plus_1_sequence(n):
+def get_3n_plus_1_cycle_length(n):
     if n == 1:
-        return [1]
+        return 1
     if n % 2 == 0:
-        return [n] + get_3n_plus_1_sequence(n // 2)
+        return 1 + get_3n_plus_1_cycle_length(n // 2)
     else:
-        return [n] + get_3n_plus_1_sequence(3 * n + 1)
+        return 1 + get_3n_plus_1_cycle_length(3 * n + 1)
 
 
 def main():
     for line in sys.stdin:
-        i, j = map(int, line.split())    # For each input line
-        sequences = []
-        for n in range(i, j + 1):
-            sequences.append(get_3n_plus_1_sequence(n))
-        max_cycle_length = max(map(len, sequences))
+        i, j = map(int, line.split())
+        sequences_length = []
+        for n in range(min([i, j]), max(i, j) + 1):
+            sequences_length.append(get_3n_plus_1_cycle_length(n))
+        max_cycle_length = max(sequences_length)
         print(f"{i} {j} {max_cycle_length}")
 
 
