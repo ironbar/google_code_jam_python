b35e51e9a8483413405fa2a7a7a1fd0455c75b2b
gbarbadillo <gbarbadillo@das-nano.com>
2023-01-15 12:02:26 +0100

    it seems that the time complexity of my algorithm is correct

--- challenges/2020/round_1a/square_dance/test2.py
+++ challenges/2020/round_1a/square_dance/test2.py
@@ -4,12 +4,25 @@ import numpy as np
 
 from code import get_competition_interest
 
+
 np.random.seed(7)
-dance_floor = np.random.randint(1, int(1e6), size=(1000, 1000))
-start = time.time()
-print(get_competition_interest(dance_floor))
-print(time.time() - start)
+for cols, rows in [(10, 10), (10, 20), (20, 20), (20, 40), (40, 40), (40, 80), (80, 80)]:
+    times = []
+    for _ in range(100):
+        dance_floor = np.random.randint(1, int(1e6), size=(rows, cols))
+        start = time.time()
+        get_competition_interest(dance_floor)
+        times.append(time.time() - start)
+    average_time = sum(times) / len(times) * 1000
+    print(f"Average time for {cols}x{rows} dance floor: {average_time:.1e} ms, time constant: {average_time / (rows * cols):.1e} ms")
+
 """
-Initial implementation takes 2.43 seconds
-Replacing the convolutions by additions reduces the time to 1.42 seconds, but it's not enough to pass the challenge
+With random dance floors the order is O(RC)
+Average time for 10x10 dance floor: 2.8e+00 ms, time constant: 2.8e-02 ms
+Average time for 10x20 dance floor: 4.7e+00 ms, time constant: 2.3e-02 ms
+Average time for 20x20 dance floor: 6.9e+00 ms, time constant: 1.7e-02 ms
+Average time for 20x40 dance floor: 1.2e+01 ms, time constant: 1.4e-02 ms
+Average time for 40x40 dance floor: 1.7e+01 ms, time constant: 1.1e-02 ms
+Average time for 40x80 dance floor: 2.6e+01 ms, time constant: 8.0e-03 ms
+Average time for 80x80 dance floor: 3.9e+01 ms, time constant: 6.2e-03 ms
 """
--- challenges/2020/round_1a/square_dance/test2_v2.py
+++ challenges/2020/round_1a/square_dance/test2_v2.py
@@ -5,11 +5,23 @@ import random
 from code_v2 import get_competition_interest
 
 random.seed(7)
-dance_floor = [[random.randint(1, int(1e6)) for _ in range(1000)] for _ in range(1000)]
-start = time.time()
-assert get_competition_interest(dance_floor) == 1374291958697
-print(time.time() - start)
+for cols, rows in [(10, 10), (10, 20), (20, 20), (20, 40), (40, 40), (40, 80), (80, 80)]:
+    times = []
+    for _ in range(100):
+        dance_floor = [[random.randint(1, int(1e6)) for _ in range(cols)] for _ in range(rows)]
+        start = time.time()
+        get_competition_interest(dance_floor)
+        times.append(time.time() - start)
+    average_time = sum(times) / len(times) * 1000
+    print(f"Average time for {cols}x{rows} dance floor: {average_time:.1e} ms, time constant: {average_time / (rows * cols):.1e} ms")
+
 """
-Initial implementation takes 8.46 seconds both with python and pypy
-Thus it is slower than the numpy implementation
+With random dance floors the order is O(RC)
+Average time for 10x10 dance floor: 5.0e-01 ms, time constant: 5.0e-03 ms
+Average time for 10x20 dance floor: 1.1e+00 ms, time constant: 5.3e-03 ms
+Average time for 20x20 dance floor: 2.2e+00 ms, time constant: 5.4e-03 ms
+Average time for 20x40 dance floor: 4.8e+00 ms, time constant: 6.0e-03 ms
+Average time for 40x40 dance floor: 1.0e+01 ms, time constant: 6.5e-03 ms
+Average time for 40x80 dance floor: 2.1e+01 ms, time constant: 6.4e-03 ms
+Average time for 80x80 dance floor: 4.3e+01 ms, time constant: 6.7e-03 ms
 """
