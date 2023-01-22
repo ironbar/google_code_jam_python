7f1d817a7fbf819249e34f3e8deedb7056936f7a
gbarbadillo <gbarbadillo@das-nano.com>
2023-01-22 08:34:22 +0100

    solved by applying a tiny change in the conditions

--- challenges/2020/round_1b/expogo/code.py
+++ challenges/2020/round_1b/expogo/code.py
@@ -12,9 +12,7 @@ def get_shortest_path(goal):
     x, y = goal
     if x == 0 and y == 0:
         return ''
-    if is_odd(x) and is_odd(y):
-        return 'IMPOSSIBLE'
-    if is_even(x) and is_even(y):
+    if is_even(x + y):
         return 'IMPOSSIBLE'
 
     if is_odd(x):
@@ -26,9 +24,9 @@ def get_shortest_path(goal):
                 return 'E' + get_shortest_path([(x - 1)//2, y//2])
         else:
             # see if we can finish
-            if x + 1 == 0:
+            if y == 0 and x + 1 == 0:
                 return 'W' + get_shortest_path([(x + 1)//2, y//2])
-            if x - 1 == 0:
+            if y == 0 and x - 1 == 0:
                 return 'E' + get_shortest_path([(x - 1)//2, y//2])
             # choose the direction that makes x odd
             if is_odd((x + 1)//2):
@@ -44,9 +42,9 @@ def get_shortest_path(goal):
                 return 'N' + get_shortest_path([x//2, (y - 1)//2])
         else:
             # see if we can finish
-            if y + 1 == 0:
+            if x == 0 and y + 1 == 0:
                 return 'S' + get_shortest_path([x//2, (y + 1)//2])
-            if y - 1 == 0:
+            if x == 0 and y - 1 == 0:
                 return 'N' + get_shortest_path([x//2, (y - 1)//2])
             # choose the direction that makes x odd
             if is_odd((y + 1)//2):
