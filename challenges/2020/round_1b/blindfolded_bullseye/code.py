7e5206e003de65ad96ae749b0bb5afa18a42ba68
gbarbadillo <gbarbadillo@das-nano.com>
2023-01-29 10:14:51 +0100

    trivial solution that passes test 1

--- challenges/2020/round_1b/blindfolded_bullseye/code.py
+++ challenges/2020/round_1b/blindfolded_bullseye/code.py
@@ -11,25 +11,18 @@ I have a limit of memory of 1GB, so I can't store all the points in memory.
 # run the script with input data
 python interactive_runner.py python3 testing_tool.py 0 -- python3 code.py
 """
-import random
+import sys
 
 def find_dartboard_center(a, b):
-    hits, misses = [], []
-    for i in range(300):
-        goal = choose_goal(a, b, hits, misses)
-        print(goal[0], goal[1])
-        result = input()
-        if result == 'CENTER':
-            return
-        elif result == 'HIT':
-            hits.append(goal)
-        elif result == 'MISS':
-            misses.append(goal)
-    return
-
-
-def choose_goal(a, b, hits, misses):
-    return random.randint(-1e9 + a, 1e9 - a), random.randint(-1e9 + a, 1e9 - a)
+    lower_limit = int(-1e9 + a)
+    upper_limit = int(1e9 - a + 1)
+    for x in range(lower_limit, upper_limit):
+        for y in range(lower_limit, upper_limit):
+            print(x, y)
+            sys.stdout.flush()
+            result = input()
+            if result == 'CENTER':
+                return
 
 
 if __name__ == '__main__':
