28fe47e91cf86ba2bf4c8a457a740edd6c5850c2
gbarbadillo <gbarbadillo@das-nano.com>
2023-02-17 21:38:23 +0100

    solved

--- challenges/2020/round_1c/overrandomized/code.py
+++ challenges/2020/round_1c/overrandomized/code.py
@@ -8,18 +8,29 @@ https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef4/0000000
 - For each server we make 10**4 queries with random M ∈ [1, 10**U - 1]
 - We are requested to recover the secret digit to letter translation
 
+The key detail is that for the test set 3 there is no information about M. Thus the distribution
+should not be random. The point is that if we concatenate two random distributions the output is not
+random. There would be more numbers starting by 1 than 2, more starting than 2 than 3 and so on.
+
 # run the script with input data
 cat input.txt | python code.py
 # run the script with input data and compare the output with the expected output
 cat input.txt | python code.py > pred.txt ; echo "Script output is: "; cat pred.txt; echo "Comparison with required output"; diff output.txt pred.txt; rm pred.txt
 """
-from functools import lru_cache
 import numpy as np
-import scipy
 
 
 def find_secret_string(queries, responses):
-    return None
+    first_letters = [response[0] for response in responses]
+    letters, counts = np.unique(first_letters, return_counts=True)
+    secret = ''.join(letters[np.argsort(counts)[::-1]])
+
+    last_letters = [response[-1] for response in responses]
+    for letter in last_letters:
+        if letter not in secret:
+            secret = letter + secret
+            break
+    return secret
 
 
 if __name__ == '__main__':
